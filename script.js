2a2c4c923486f532ecf5c0a3e256ed1102bfe1d5
Anita Chengalva <anita.chengalva@gmail.com>
2022-08-09 16:38:46 -0700

    updated variable declarations

---
+++ script.js
@@ -0,0 +1,12 @@
+// declare variables
+var start = document.getElementById("#start");
+start.onclick = startQuiz;
+var end = document.getElementById("#end");
+var finalScore = document.getElementById("#finalScore");
+
+var timer = document.getElementById("#timer");
+var message = document.getElementById("#message");
+var scoreList = document.getElementById("#scoreList");
+
+var question = document.getElementById("#question");
+var choices = document.getElementById("#choices");
